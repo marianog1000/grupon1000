e77d7ac9b48a10b50d97da6a23976b68108986ed
marianog1000 <marianog1000@hotmail.com>
2017-06-29 21:45:29 -0300

    se anularon las claves

--- public_html/wp-config.php
+++ public_html/wp-config.php
@@ -16,13 +16,13 @@
 
 // ** Ajustes de MySQL. Solicita estos datos a tu proveedor de alojamiento web. ** //
 /** El nombre de tu base de datos de WordPress */
-define('DB_NAME', 'c0620253_nina');
+define('DB_NAME', '');
 
 /** Tu nombre de usuario de MySQL */
-define('DB_USER', 'c0620253_nina');
+define('DB_USER', '');
 
 /** Tu contraseña de MySQL */
-define('DB_PASSWORD', 'sogisoLE69');
+define('DB_PASSWORD', '');
 
 /** Host de MySQL (es muy probable que no necesites cambiarlo) */
 define('DB_HOST', 'localhost');
